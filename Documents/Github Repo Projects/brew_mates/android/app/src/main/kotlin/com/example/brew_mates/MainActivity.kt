51095a2ef77c2d6fc20535c1578a3d9b4b486664
iamyassin08 <yassinmohamed49@gmail.com>
2024-05-03 23:21:46 -0500

    update

--- Documents/Github Repo Projects/brew_mates/android/app/src/main/kotlin/com/example/brew_mates/MainActivity.kt
+++ Documents/Github Repo Projects/brew_mates/android/app/src/main/kotlin/com/example/brew_mates/MainActivity.kt
@@ -1,5 +1,13 @@
-package com.example.brew_mates
+package com.example.brew_crew
 
-import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle
 
-class MainActivity: FlutterActivity()
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
