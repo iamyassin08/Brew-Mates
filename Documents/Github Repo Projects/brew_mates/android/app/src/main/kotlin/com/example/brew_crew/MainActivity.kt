39245d0119074380b760a8ebd65613c0f502611e
iamyassin08 <yassinmohamed49@gmail.com>
2024-05-04 08:57:13 -0500

    update

--- Documents/Github Repo Projects/brew_mates/android/app/src/main/kotlin/com/example/brew_crew/MainActivity.kt
+++ Documents/Github Repo Projects/brew_mates/android/app/src/main/kotlin/com/example/brew_crew/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.brew_crew
 
 import android.os.Bundle
-
++import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 
---
+++ Documents/Github Repo Projects/brew_mates/android/app/src/main/kotlin/com/thenetninja/brew_mates/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.thenetninja.brew_mates
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
